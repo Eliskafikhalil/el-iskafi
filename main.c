e5eb701c49bfd9c4831d54b68bbea0a1519bec0e
khalil <eliskafikhalil2002@gmail.com>
2023-09-13 15:58:20 +0100

    profile

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char NOM[10];
+    char Prenom[10];
+    int Age;
+    char Sex[10];
+    int Numero;
+    printf("Enter your name:\n");
+    scanf("%s",&NOM);
+
+    printf("Enter your last name:\n");
+    scanf("%s",&Prenom);
+
+    printf("Enter your age:\n");
+    scanf("%d",&Age);
+
+    printf("Enter numero :\n");
+    scanf("%d", &Numero);
+
+    printf("Enter your sex:\n");
+    scanf("%s", &Sex);
+
+
+}
+
+
